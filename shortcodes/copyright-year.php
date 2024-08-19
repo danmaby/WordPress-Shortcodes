cbf044e6363cfcccd48af791770048ace800cda7
Dan Maby <danmaby@users.noreply.github.com>
2024-08-19 18:11:29 +0100

    feat: Add copyright year shortcode

---
+++ shortcodes/copyright-year.php
@@ -0,0 +1,28 @@
+<?php
+
+// Register the shortcode
+function display_copyright_years($atts)
+{
+  // Extract attributes and set a default starting year
+  $atts = shortcode_atts(
+    [
+      "start_year" => "2020", // Default start year if not provided
+    ],
+    $atts
+  );
+
+  // Get the current year
+  $current_year = date("Y");
+
+  // Build the copyright string
+  $start_year = intval($atts["start_year"]);
+  if ($start_year == $current_year) {
+    $copyright = "© $current_year";
+  } else {
+    $copyright = "© $start_year–$current_year";
+  }
+
+  // Return the copyright string
+  return $copyright;
+}
+add_shortcode("copyright_years", "display_copyright_years");
